4ccdc71ecd1c82370f874a84d9ca5a36beb7713f
barsci <barsci@o2.pl>
2019-01-14 13:40:03 +0100

    Added action listener

--- src/main/java/com/gui/ButtonPanel.java
+++ src/main/java/com/gui/ButtonPanel.java
@@ -1,7 +1,23 @@
 package com.gui;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.model.v251.message.ADT_A01;
+import ca.uhn.hl7v2.model.v251.segment.MSH;
+import ca.uhn.hl7v2.model.v251.segment.PID;
+import ca.uhn.hl7v2.parser.Parser;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 import javax.swing.*;
 
@@ -9,10 +25,9 @@ public class ButtonPanel extends JFrame{
 
     private static final int HEIGHT = 300;
     private static final int WIDTH = 300;
-    private File toSave;
     private JButton greenButton;
     private JFrame buttonPanel;
-    static JTextField[] textFields = new JTextField[6];
+    private JTextField[] textFields = new JTextField[6];
     private JLabel[] labels = new JLabel[6];
     private String[] labelNames = new String[]{"Imie", "Nazwisko","Adres", "Pesel", "Numer konta", "Nazwa pliku do zapisu"};
     public ButtonPanel() {
@@ -34,5 +49,48 @@ public class ButtonPanel extends JFrame{
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         add(greenButton);
         setVisible(true);
+
+        greenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ADT_A01 adt = new ADT_A01();
+                try {
+                    adt.initQuickstart("ADT", "A01", "P");
+                } catch (ca.uhn.hl7v2.HL7Exception | IOException exc) {
+                    System.out.println(exc.toString());
+                }
+
+                MSH mshSegment = adt.getMSH();
+                try {
+                    mshSegment.getSendingApplication().getNamespaceID().setValue("PatientDelivery");
+                    mshSegment.getSequenceNumber().setValue("293");
+                } catch (ca.uhn.hl7v2.model.DataTypeException exc) {
+                    System.out.println(exc.toString());
+                }
+
+                PID pid = adt.getPID();
+                try {
+                    pid.getPatientName(0).getGivenName().setValue(textFields[0].getText());
+                    pid.getPatientName(0).getFamilyName().getSurname().setValue(textFields[1].getText());
+                    pid.getPatientAddress(0).getAddressRepresentationCode().setValue(textFields[2].getText());
+                    pid.getPatientIdentifierList(0).getIDNumber().setValue(textFields[3].getText());
+                    pid.getPatientAccountNumber().getIDNumber().setValue(textFields[4].getText());
+                } catch (ca.uhn.hl7v2.model.DataTypeException exc) {
+                    System.out.println(exc.toString());
+                }
+
+                HapiContext context = new DefaultHapiContext();
+                Parser parser = context.getPipeParser();
+                String filename = "F:\\" + textFields[textFields.length - 1].getText();
+                try {
+                    String encodedMessage = parser.encode(adt);
+                    final Path path = Paths.get(filename);
+                    Files.write(path, Arrays.asList(encodedMessage), StandardCharsets.UTF_8,
+                            Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+                } catch (final IOException | ca.uhn.hl7v2.HL7Exception exc) {
+                    System.out.println(exc.toString());
+                }
+            }
+        });
     }
 }
